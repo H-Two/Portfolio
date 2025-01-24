dd87a2c06b7db10cd5cc831f933e3151444d8bf1
Hai Do <haihoangdovn05@gmail.com>
2025-01-23 22:04:52 -0500

    Display name header

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -3,11 +3,27 @@ import './style/App.css'
 function App() {
   return (
     <>
-      <p className="read-the-docs">
-        DU M4
-      </p>
+      <div className='nav-bar'>
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>LinkedIn</li>
+          <li>CV</li>
+        </ul>
+      </div>
+      <div className='info'>
+        <h1>
+          Hai Hoang Do
+        </h1>
+        <h3>
+          Computer Science Student
+        </h3>
+        <h2>
+          Third year Computer Science at Dawson College. <br/> Passionate and eager to pursue a career in the tech industry.
+        </h2>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
