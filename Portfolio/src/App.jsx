66045dbf8a2c1add3b7fb416b3efa1a5adc8c7cb
HaiHoang_Do <hoang.do@dawsoncollege.qc.ca>
2025-01-24 10:26:42 -0800

    Display skill block

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -5,23 +5,48 @@ function App() {
     <>
       <div className='nav-bar'>
         <ul>
-          <li>Home</li>
-          <li>Projects</li>
-          <li>LinkedIn</li>
-          <li>CV</li>
+          <li><a>Home</a></li>
+          <li><a>Projects</a></li>
+          <li><a href="https://www.linkedin.com/in/hai-hoang-do-b65b18339/" target="_blank">LinkedIn</a></li>
+          <li><a>CV</a></li>
         </ul>
       </div>
-      <div className='info'>
-        <h1>
-          Hai Hoang Do
-        </h1>
+      <section className='info'>
+        <h1>Hai Hoang Do</h1>
+        <p>Montreal, Canada<br/>haihoangdovn05@gmail.com</p>
         <h3>
-          Computer Science Student
+          Third year Computer Science student at Dawson College. <br/> Passionate and eager to pursue a career in the tech industry.
         </h3>
-        <h2>
-          Third year Computer Science at Dawson College. <br/> Passionate and eager to pursue a career in the tech industry.
-        </h2>
-      </div>
+      </section>
+      <section className='skills'>
+        <h2>SKILLS</h2>
+        <div className='skill-container'>
+          <div className='skill-block'>
+            <h3>JavaScript</h3>
+          </div>
+          <div className='skill-block'>
+            <h3>React</h3>
+          </div>
+          <div className='skill-block'>
+            <h3>C#</h3>
+          </div>
+          <div className='skill-block'>
+            <h3>Python</h3>
+          </div>
+          <div className='skill-block'>
+            <h3>Java</h3>
+          </div>
+          <div className='skill-block'>
+            <h3>Kotlin</h3>
+          </div>
+        </div>
+      </section>
+      <section className='experience'>
+        <h2>EXPERIENCE</h2>
+      </section>
+      <section className='projects'>
+        <h2>PROJECTS</h2>
+      </section>
     </>
   );
 }
