e29dcb9d96b22cad4dee6f9761d7acf0c25408e0
Hai Do <haihoangdovn05@gmail.com>
2025-01-25 21:48:17 -0500

    Display project screenshot

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -43,9 +43,38 @@ function App() {
       </section>
       <section className='experience'>
         <h2>EXPERIENCE</h2>
+        <h4>Computer Service Technician</h4>
+        <h4>Canada Computers</h4>
+        <ul>
+          <li>Build, test, and maintain computer systems and related products</li>
+        </ul>
       </section>
       <section className='projects'>
         <h2>PROJECTS</h2>
+        <div className='project-block'>
+          <h4>Office Hour Scheduler</h4>
+          <div className='project-info'>
+            <ul>
+              <li>Full stack Python & Flask web application</li>
+              <li>Oracle database used for data collection</li>
+              <li>Communication platform for managing appointments and reports</li>
+              <li>Provides user authentication for different user type</li>
+            </ul>
+            <img src='./src/assets/scheduler.svg' alt='Scheduler'/>
+          </div>
+        </div>
+        <div className='project-block'>
+          <h4>CO2 Emission & Natural Disasters Visualizer</h4>
+          <div className='project-info'>
+            <ul>
+              <li>Full stack React web application</li>
+              <li>Utilizes a MongoDB cloud database</li>
+              <li>Displays CO2 emission and natural disasters datasets over maps and graphs</li>
+              <li>Provides REST API for data retrieval hosted with Express.js</li>
+            </ul>
+            <img src='./src/assets/emission.svg' alt='Scheduler'/>
+          </div>
+        </div>
       </section>
     </>
   );
